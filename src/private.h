1f3d7a99d9a69fa6d331b8a357e29dfe1f194d01
larryl79 <larryl79@gmail.com>
2022-03-16 18:40:51 +0000

    Add files via upload

---
+++ src/private.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define HOSTNAME          ""
+#define CLIENTSSID        ""               //wi-fi netwrok name
+#define CLIENTPASSWORD    ""         //wi-fi network password
+#define AP_SSID           ""  // Standalone AP SSID
+#define AP_PASS           ""          // Standalone AP password
